2b0389c0789c8d011792f6a8c88417144004d072
restrain0327 <2685315019@qq.com>
2024-05-08 12:34:59 +0800

    24.5.7 完成第392题 之前的忘记推了

---
+++ src/动态规划/Solution_115.java
@@ -0,0 +1,10 @@
+package 动态规划;
+
+/**
+ * 文件描述：
+ *
+ * @Author : restrain
+ * @CreateDate 2024/5/8 12:34
+ */
+public class Solution_115 {
+}
--- src/动态规划/Solution_392.java
+++ src/动态规划/Solution_392.java
@@ -8,6 +8,36 @@ package 动态规划;
  */
 public class Solution_392 {
     public boolean isSubsequence(String s, String t) {
+        // 392 判断子序列
+        // len1 < len2
 
+        int len1 = s.length();
+        int len2 = t.length();
+
+        if (len1 > len2){
+            return false;
+        }
+
+        int[][] dp = new int[len1+1][len2+1];
+
+        // 从一开始不用初始化 后面会覆盖 而且一定要写等于
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else {
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+//                System.out.println(dp[i][j]);
+            }
+        }
+//        System.out.println(dp[len1][len2]);
+        return dp[len1][len2] == len1;
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbgdc";
+        System.out.println(new Solution_392().isSubsequence(s, t));
     }
 }
