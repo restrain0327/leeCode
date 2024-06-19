409470a4e8f57ec4f83cfe779a4bb572cfb62e32
restrain0327 <2685315019@qq.com>
2024-06-19 18:24:41 +0800

    24.6.17 链表143 用集合保存节点 在重排链表 注意最后要指向null 防止成环

--- src/练习/链表/Solution_143.java
+++ src/练习/链表/Solution_143.java
@@ -1,5 +1,7 @@
 package 练习.链表;
 
+import java.util.ArrayList;
+
 /**
  * 文件描述：
  *
@@ -8,6 +10,37 @@ package 练习.链表;
  */
 public class Solution_143 {
     public void reorderList(ListNode head) {
-        //
+        // 重排链表
+        // 结合数组 集合重排
+
+        ArrayList<ListNode> nodeList = new ArrayList<>();
+        ListNode cur = head;
+        // 添加进来
+        while (cur != null){
+            nodeList.add(cur);
+            cur = cur.next;
+        }
+
+        // 初始化
+        cur = head;
+
+        int left = 1, right = nodeList.size()-1;
+        int num = 1;
+        while (left <= right){
+            if (num % 2 == 0){
+                // 偶数
+                cur.next = nodeList.get(left);
+                left++;
+            }else {
+                //奇数
+                cur.next = nodeList.get(right);
+                right--;
+            }
+            num++;
+            cur = cur.next;
+        }
+
+        // 防止成环
+        cur.next = null;
     }
 }
